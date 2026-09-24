677afe61a82b05e9c11ff2c109021fbddfe74644
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246733.cfp127aluno.local>
2026-09-24 16:59:54 -0300

    feat: enviar edições

--- app/src/main/java/com/example/idade/MainActivity.kt
+++ app/src/main/java/com/example/idade/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.idade.ui.theme.IdadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +34,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IdadeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +45,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
-}
+            .fillMaxSize()
+            .background(Color(0xFFF9F8FC)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Qual é a sua idade?",
+            fontSize = 28.sp,
+            color = Color(0xFF4958B1)
+        )
+        Text(
+            text = "Aperte os botões para informar sua idade",
+            fontSize = 16.sp,
+            color = Color(0xFF313138)
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IdadeTheme {
-        Greeting("Android")
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
+
+        Text(
+            text = "10",
+            fontSize = 30.sp,
+            color = Color(0xFF2F3238)
+        )
+
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .size(60.dp),
+                shape = RoundedCornerShape(
+                    topStart = 10.dp,
+                    topEnd = 10.dp,
+                    bottomStart = 10.dp,
+                    bottomEnd = 10.dp
+                )
+            ) {
+                Text(
+                    text = "-",
+
+                )
+            }
+        }
     }
 }
